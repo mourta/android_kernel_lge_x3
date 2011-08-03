fdc0533606d1b2f1bf83d94f3fe1b635a8adf9b1
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2011-08-03 03:34:24 -0700

    rcu: Dump local stack if cannot dump all CPUs' stacks
    The trigger_all_cpu_backtrace() function is a no-op in architectures that
    do not define arch_trigger_all_cpu_backtrace.  On such architectures, RCU
    CPU stall warning messages contain no stack trace information, which makes
    debugging quite difficult.  This commit therefore substitutes dump_stack()
    for architectures that do not define arch_trigger_all_cpu_backtrace,
    so that at least the local CPU's stack is dumped as part of the RCU CPU
    stall warning message.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcutree.c
+++ kernel/rcutree.c
@@ -583,7 +583,8 @@ static void print_other_cpu_stall(struct rcu_state *rsp)
 	}
 	printk("} (detected by %d, t=%ld jiffies)\n",
 	       smp_processor_id(), (long)(jiffies - rsp->gp_start));
-	trigger_all_cpu_backtrace();
+	if (!trigger_all_cpu_backtrace())
+		dump_stack();
 
 	/* If so configured, complain about tasks blocking the grace period. */
 
@@ -602,13 +603,10 @@ static void print_cpu_stall(struct rcu_state *rsp)
 	 * See Documentation/RCU/stallwarn.txt for info on how to debug
 	 * RCU CPU stall warnings.
 	 */
-	printk(KERN_ERR "INFO: %s detected stall on CPU %d (t=%lu jiffies %lu ms)\n",
-	       rsp->name, smp_processor_id(), jiffies - rsp->gp_start, jiffies_to_msecs(jiffies - rsp->gp_start) );
-
-/*                                                                                                                   */
-	panic("need to reset because of detecting stall on CPU\n");
-	//trigger_all_cpu_backtrace();
-/*                                                                                                                   */
+	printk(KERN_ERR "INFO: %s detected stall on CPU %d (t=%lu jiffies)\n",
+	       rsp->name, smp_processor_id(), jiffies - rsp->gp_start);
+	if (!trigger_all_cpu_backtrace())
+		dump_stack();
 
 	raw_spin_lock_irqsave(&rnp->lock, flags);
 	if (ULONG_CMP_GE(jiffies, rsp->jiffies_stall))

c830f5c6990ca595a7e3e6e25a1b4235f374e2ed
demetris <ierokipides.dem@gmail.com>
2014-02-07 20:01:35 +0200

    I was suspecting this, we are an Enterprise board derivative

--- arch/arm/mach-tegra/lge/x3/board-x3-sdhci.c
+++ arch/arm/mach-tegra/lge/x3/board-x3-sdhci.c
@@ -579,17 +579,17 @@ static int x3_wifi_reset(int on)
 //                                                       
 
 #ifdef CONFIG_TEGRA_PREPOWER_WIFI
-static int __init enterprise_wifi_prepower(void)
+static int __init x3_wifi_prepower(void)
 {
-	if (!machine_is_tegra_enterprise())
+	if (!machine_is_tegra_x3())
 		return 0;
 
-	enterprise_wifi_power(1);
+	x3_wifi_power(1);
 
 	return 0;
 }
 
-subsys_initcall_sync(enterprise_wifi_prepower);
+subsys_initcall_sync(x3_wifi_prepower);
 #endif
 
 
